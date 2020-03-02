11e0d546a3c26c8f546e2be392d7bed684b7b778
Anton Boovyk <antonbovik@gmail.com>
2020-03-02 14:21:47 +0200

    Initial commit

---
+++ src/Laboratorna.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+import java.lang.String;
+import java.util.ArrayList;
+
+public class Laboratorna {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(new File("premier_league1.csv"));
+        String[] splitted;
+        String digits;
+        String line_first;
+        line_first = sc.nextLine();
+        int k = Integer.parseInt(String.valueOf(line_first));
+        String[][] BoardOfResults = new String[k][5];
+        int count = 0;
+        while (sc.hasNext()) {
+            ArrayList<Integer> arrayList = new ArrayList<>();
+            splitted = sc.nextLine().split(",|:");
+            for (int i = 1; i < splitted.length; i++) {
+                int a = Integer.parseInt(splitted[i]);
+                arrayList.add(a);
+            }
+
+        }
+    }
+}
